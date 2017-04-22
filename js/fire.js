218a63e7b75da61d59ed1676291de31cad6f58b4
ijos0001 <ijos0001@student.monash.edu>
2017-04-22 17:36:30 +1000

    Updated UI Design

---
+++ js/fire.js
@@ -0,0 +1,57 @@
+var config = {
+    apiKey: "AIzaSyBLA6sDqXwnMX6tJayM4dmYCPRzt5CW91M",
+    authDomain: "learntogether-a250b.firebaseapp.com",
+    databaseURL: "https://learntogether-a250b.firebaseio.com",
+    projectId: "learntogether-a250b",
+    storageBucket: "learntogether-a250b.appspot.com",
+    messagingSenderId: "3175571907"
+};
+firebase.initializeApp(config);
+
+signOut = function () {
+    firebase.auth().signOut().then(function() {
+        console.log("Signed out")
+    }, function(error) {
+        // An error happened.
+    });
+};
+
+initApp = function() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var uid = user.uid;
+            
+            user.getToken().then(function(accessToken) {
+                if(email.indexOf('monash.edu') < 0){
+                    alert("Use monash email");
+                    signOut();
+                }else{
+                    //redirect
+                    window.location.replace("");
+                }
+            });
+
+        } else {
+            // User is signed out.
+
+        }
+    }, function(error) {
+        console.log(error);
+    });
+};
+
+
+var uiConfig = {
+    signInSuccessUrl: '.',
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>'
+};
+
